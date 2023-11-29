dd8bec154b08c24801d9819466b94e2ee11b26ac
Gustavo Ortiz <gustavoortiz@me.com>
2023-11-29 13:09:21 +0100

    Update

--- src/my_package/add_columns.py
+++ src/my_package/add_columns.py
@@ -3,7 +3,18 @@ import pandas as pd
 import dask.dataframe as dd
 
 
+
 def create_time_column(df):
+    '''
+    Takes Dataframe with separate columns for hour, minute, second and millisecond and returns Dataframe with a single
+    time column.
+
+    :param df: dataframe for which to calculate time column
+    :type df: pandas.DataFrame
+
+    :return: dataframe with dedicated time column
+    :rtype: pandas.DataFrame
+    '''
     '''
     Takes Dataframe with separate columns for hour, minute, second and millisecond and returns Dataframe with a single
     time column.
@@ -21,6 +32,8 @@ def create_time_column(df):
     df.columns = custom_header_names
 
     # Necessary for format but can be chosen arbitrarily
+
+    # Necessary for format but can be chosen arbitrarily
     default_year = 2023
     default_month = 1
     default_day = 1
@@ -28,6 +41,7 @@ def create_time_column(df):
     df.reset_index(drop=True)
 
     # Create a new datetime column
+    df['time'] = pd.to_datetime({
     df['time'] = pd.to_datetime({
         'year': default_year,
         'month': default_month,
@@ -38,9 +52,11 @@ def create_time_column(df):
         'millisecond': df['millisecond']
     }, errors='raise')
     # Drop old columns containing information on time
+    # Drop old columns containing information on time
     columns_to_drop = ['hour', 'minute', 'second', 'millisecond']
     df = df.drop(columns=columns_to_drop)
 
+    # Change column order
     # Change column order
     new_column_order = ['id', 'time', 'x', 'y']
     df_time = df[new_column_order]
@@ -62,13 +78,29 @@ def create_rul_columns(df):
     '''
 
     # Get max/min values in time column for each id individually
+    '''
+    Takes a Dataframe and returns it with additional RUL columns.
+
+    Creates column that contains RUL scaled to [0,1] and a column that contains a RUL class.
+
+    :param df: pd.dataframe for which RUL columns shall be created
+    :type df: pandas.DataFrame
+
+    :return:
+    :rtype: pandas.DataFrame
+    '''
+
+    # Get max/min values in time column for each id individually
     max_values = df.groupby('bearing_id')['time'].max()
     min_values = df.groupby('bearing_id')['time'].min()
 
     # Calculate time differences
+    # Calculate time differences
     df['date_time_difference'] = max_values[1] - df['time']
     df['max_date_time_difference'] = max_values[1] - min_values[1]
 
+    # Calculate RUL and RUL_class
+    # TODO: Decide on RUL Classes
     # Calculate RUL and RUL_class
     # TODO: Decide on RUL Classes
     df['RUL'] = df['date_time_difference'] / df['max_date_time_difference']
@@ -82,6 +114,18 @@ def create_rul_columns(df):
 
 
 def create_id_column(df, id_value):
+    '''
+
+    :param df: input dataframe
+    :type df: pandas.DataFrame
+
+    :param id_value: id
+    :type id_value: int
+
+    :return: dataframe with additional id column
+    :rtype: pandas.DataFrame
+    '''
+
     '''
 
     :param df: input dataframe
